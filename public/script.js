b6546b43b3b0eee5a3e3415b7d8f913c9d8ab28c
Bob Jarvis <bob@zeal.co>
2020-04-27 19:45:16 +0930

    fixed supporter links

--- public/script.js
+++ public/script.js
@@ -1,35 +1,9 @@
-// var icons = [
-//   // {
-//   //   "url": "https://vizzable.zeal.co",
-//   //   "img": "vizzable.png"
-//   // },
-
-//   {
-//     "url": "https://resolume.com",
-//     "img": "resolume.png"
-//   },
-
-//   {
-//     "url": "https://cycling74.com",
-//     "img": "max.png"
-//   },
-
-//   {
-//     "url": "https://processing.org",
-//     "img": "processing.png"
-//   }
-
-// ];
-
+//Adds logos and supporter shout-outs
 document.body.onload = addFriends;
 
 function addFriends() {
 	//add icons
 	icons.forEach(function(icon) {
-		// console.log(icon.img);
-
-		var friend = document.createElement('div');
-		friend.className = 'friend';
 
 		var img = document.createElement('img');
 		img.src = 'images/icons/' + icon.img;
@@ -37,33 +11,27 @@ function addFriends() {
 		img.style.maxWidth = '60px';
 
 		var link = document.createElement('a');
-		link.href = icon.url;
-
-		link.appendChild(img);
+    link.href = icon.url;
+    link.appendChild(img);
+    
+    var friend = document.createElement('div');
+		friend.className = 'friend';
 		friend.appendChild(link);
 
-		var friendsDiv = document.getElementById('friends').appendChild(friend);
+    document.getElementById('friends').appendChild(friend);
+    
 	});
 
-  //add supporters
-  var supportersDiv = document.getElementById('supporters');
-
+	//add supporters
 	supporters.forEach(function(supporter) {
-		console.log(supporter.name);
 
-		var supporter = document.createTextNode(supporter.name + ' \u00A0\u00A0');
-
-		// var img = document.createElement('img');
-		// img.src = 'images/icons/' + icon.img;
-		// img.style.maxHeight = '60px';
-		// img.style.maxWidth = '60px';
-
-		var link = document.createElement('a');
+  	var supporterText = document.createTextNode(supporter.name + ' \u00A0\u00A0');
+    
+    var link = document.createElement('a');
 		link.href = supporter.url;
+    link.appendChild(supporterText);
+  
+    document.getElementById('supporters').appendChild(link);  
 
-		link.appendChild(supporter);
-		// friend.appendChild(link);
-
-		supportersDiv.appendChild(link);
 	});
 }
