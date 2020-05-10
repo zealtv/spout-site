29c3e0d727cbd5a3a394b5ac255b8f4dae4f315f
Bob Jarvis <bob@zeal.co>
2020-05-10 19:45:44 +0930

    colour scheme changes, icons added

--- public/logos.js
+++ public/logos.js
@@ -1,21 +1,239 @@
 var icons = [
-	// {
-	//   "url": "https://vizzable.zeal.co",
-	//   "img": "vizzable.png"
-	// },
 
 	{
-		url: 'https://resolume.com',
-		img: 'resolume.png'
+		url: 'https://processing.org',
+		img: 'Processing_128.png'
 	},
 
 	{
 		url: 'https://cycling74.com',
-		img: 'max.png'
+		img: 'Max6_128.png'
+	},
+	
+	{
+		url: 'http://openframeworks.cc',
+		img: 'OF_128.png'
+	},
+	
+	{
+		url: 'http://libcinder.org',
+		img: 'cinder_460.png'
+	},
+	
+	{
+		url: 'http://vvvv.org',
+		img: 'vvvv_512.png'
+	},
+	
+	{
+		url: 'https://github.com/sloopidoopi/Spout4Unity',
+		img: 'Unity_128.png'
+	},
+	
+	{
+		url: 'https://github.com/benkuper/Spout-AfterEffects',
+		img: 'After_Effects_128.png'
+	},
+	
+	{
+		url: 'https://github.com/benkuper/AIR-NativeExtensions/tree/master/Spout2AIR',
+		img: 'Adobe_AIR.png'
 	},
 
 	{
-		url: 'https://processing.org',
-		img: 'processing.png'
+		url: 'https://github.com/maybites/blender.script.spout',
+		img: 'Blender_512.png'
+	},
+
+	{
+		url: 'https://resolume.com',
+		img: 'arena7icon.png'
+	},
+
+	{
+		url: 'http://derivative.ca',
+		img: 'TouchDesigner_128.png'
+	},
+	
+	{
+		url: 'http://vj-dj.arkaos.net/',
+		img: 'GrandVJ_128.png'
+	},
+	
+	{
+		url: 'https://heavym.net/en/',
+		img: 'HeavyM_128.png'
+	},
+	
+	{
+		url: 'http://troikatronix.com/',
+		img: 'Isadora_128.png'
+	},
+	
+	{
+		url: 'http://www.madmapper.com',
+		img: 'MadMapper_128.png'
+	},
+	
+	{
+		url: 'http://magicmusicvisuals.com',
+		img: 'Magic_128.png'
+	},
+	
+	{
+		url: 'https://www.mxwendler.net',
+		img: 'MXWendler_128.png'
+	},
+	
+	{
+		url: 'http://www.bigfug.com/software/painting-with-light',
+		img: 'PaintingWithLight_128.png'
+	},
+	
+	{
+		url: 'http://www.mixvibes.com/remixvideo-vj-software',
+		img: 'Remixvideo_128.png'
+	},
+	
+	{
+		url: 'https://tecartlab.com/',
+		img: 'SPARCK_128.png'
+	},
+	
+	{
+		url: 'https://synesthesia.live',
+		img: 'Synesthesia_128.png'
+	},
+
+	{
+		url: 'http://www.zwobotmax.com',
+		img: 'Zwobot_128.png'
+	},
+	
+	{
+		url: 'https://www.enttec.com/?main_menu=Products&pn=70581',
+		img: 'ELM_128.png'
+	},
+	
+	{
+		url: 'http://smode.fr/synth',
+		img: 'SmodeLogo128.png'
+	},
+	
+	{
+		url: 'https://www.green-hippo.com',
+		img: 'gh_logo_256.png'
+	},
+	
+	{
+		url: 'http://hexler.net/software/kodelife',
+		img: 'KodeLife-icon.png'
+	},
+	
+	{
+		url: 'https://github.com/videodromm',
+		img: 'videodromm.png'
+	},
+	
+	{
+		url: 'http://visution.com/mapio',
+		img: 'mapio-icon.png'
+	},
+	
+	{
+		url: 'https://www.avolites.com/ai-features',
+		img: 'avolites-logo_512.png'
+	},
+	
+	{
+		url: 'http://www.modulo-pi.com/en',
+		img: 'modulo-pi_640.png'
+	},
+	
+	{
+		url: 'https://www.7thsensedesign.com',
+		img: '7thsense-design-700.png'
+	},
+	
+	{
+		url: 'http://www.simmetri.com/',
+		img: 'simmetri_400.png'
+	},
+	
+	{
+		url: 'http://www.videomappingsoftware.com',
+		img: 'tabulatouch_640.png'
+	},
+	
+	{
+		url: 'https://gethapmap.com/en/',
+		img: 'hapmap_circle.png'
+	},
+	
+	{
+		url: 'https://github.com/leadedge/Spout2/tree/master/VIRTUALDJ',
+		img: 'VirtualDJ_600.png'
+	},
+	
+	{
+		url: 'https://github.com/leadedge/Spout2/tree/master/MILKDROP',
+		img: 'winamp_512.png'
+	},
+	
+	{
+		url: 'https://lightact-systems.com',
+		img: 'Lightact-logo_673.png'
+	},
+	
+	{
+		url: 'https://www.ventuz.com',
+		img: 'VentuzLogo_512.png'
+	},
+	
+	{
+		url: 'https://www.vyv.ca',
+		img: 'VYV_512.png'
+	},
+	
+	{
+		url: 'http://www.hirender.cn/en/index.html#',
+		img: 'Hirender_512.png'
+	},
+	
+	{
+		url: 'http://nestimmersion.ca',
+		img: 'nestimmersion_256.png'
+	},
+	
+	{
+		url: 'http://www.ultragrid.cz/',
+		img: 'ultragrid_logo_128.png'
+	},
+	
+	{
+		url: 'https://www.praxislive.org',
+		img: 'praxis_live-256.png'
+	},
+	
+	{
+		url: 'https://www.notch.one',
+		img: 'notch_256.png'
+	},
+	
+	{
+		url: 'https://ni-mate.com',
+		img: 'ni-mate-logo_256.png'
+	},
+	
+	{
+		url: 'http://www.vioso.com',
+		img: 'VIOSO_300.png'
+	},
+	
+	{
+		url: 'http://www.visikord.com',
+		img: 'visikord_114.png'
 	}
+	
 ];
+
